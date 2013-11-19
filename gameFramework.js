80267290d8385fa6cb7bc3c2425d7fa8ad2a9c11
techbot <djtechbot@gmail.com>
2013-11-19 20:50:13 +0000

    template files

---
+++ gameFramework.js
@@ -0,0 +1,18 @@
+$(function(){
+
+
+    
+    $("#startButton").click(function() {
+      
+        $("#startButton").remove();
+        $("#mygame").append("<div id='sprite1'>");
+        $("#sprite1").css("backgroundImage","url('spritesheet1.png')");
+   
+   
+   
+   
+   
+   
+   
+    });
+});
